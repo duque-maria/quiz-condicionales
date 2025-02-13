1855267a5924df865894fea660f785a1d1663fd7
duque-maria <mariangelduque@colegioguanenta.edu.co>
2025-02-13 15:00:58 -0500

    se agregó el diagrama

---
+++ quiz.py
@@ -0,0 +1,18 @@
+import math
+
+# Quiz instrucciones condicionales
+"""Dados tres números a, b y c, correspondientes a la longitud de los lados de una figura geométrica, determinar si pueden formar los lados de un triángulo."""
+
+print("------------------------------")
+print("----- VERIFICAR TRIANGULO ----")
+print("------------------------------")
+
+# input
+a = int(input("Digite el primer valor: "))
+b = int(input("Digite el segundo valor: "))
+c = int(input("Digite el tercer valor: "))
+
+# processing
+
+
+# output
