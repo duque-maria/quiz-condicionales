789fd263bcc592aab835e3980ee04082920c3c60
duque-maria <mariangelduque@colegioguanenta.edu.co>
2025-02-13 15:24:29 -0500

    se agregó el programa en python

--- quiz.py
+++ quiz.py
@@ -13,6 +13,15 @@ b = int(input("Digite el segundo valor: "))
 c = int(input("Digite el tercer valor: "))
 
 # processing
-
+if a + b > c:
+    r= "Si pueden formar los lados de un triángulo"
+if a + c > b:
+    r= "Si pueden formar los lados de un triángulo"
+if b + c > a:
+    r= "Si pueden formar los lados de un triángulo"
+else:
+     r= "No se puede formar un triángulo"
 
 # output
+print("························")
+print ("La respuesta es que: " + str(r))
